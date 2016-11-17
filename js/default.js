3ff734194765ac15987b57ff3e5ebb80992f7e00
Carolina Caires <carolbazarello@gmail.com>
2016-11-17 16:08:31 +0000

    Troca versão do jquery e coloca animação nos progress bar

---
+++ js/default.js
@@ -0,0 +1,19 @@
+$(document).ready(function() {
+
+    $("#php.progress-bar").animate({
+        width: "70%"
+    }, 2500);
+
+    $("#mysql.progress-bar").animate({
+        width: "50%"
+    }, 2500);
+
+    $("#js.progress-bar").animate({
+        width: "80%"
+    }, 2500);
+
+    $("#git.progress-bar").animate({
+        width: "65%"
+    }, 2500);
+
+});
